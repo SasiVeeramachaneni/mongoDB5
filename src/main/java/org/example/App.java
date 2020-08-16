51e752a33db3d8cf6ee7cd2ec882cb6c734c2297
SasiVeeramachaneni <54708043+SasiVeeramachaneni@users.noreply.github.com>
2020-08-16 11:41:04 +0530

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,35 +1,20 @@
 package org.example;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import org.bson.Document;
-import org.bson.codecs.configuration.CodecRegistry;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        CodecRegistry pojoCodecRegistry;
 
-        ConnectionString connString = new ConnectionString(
-                "mongodb+srv://dbUser:dbPassword@cluster0-1bxg9.mongodb.net/");
-
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(connString)
-                .retryWrites(true)
-                .build();
-        MongoClient mongoClient = MongoClients.create(settings);
-        MongoDatabase database = mongoClient.getDatabase("sample_analytics");
-        MongoCollection<Document> coll = database.getCollection("accounts");
+        Connection connectToDB = new Connection();
+        MongoCollection<Document> coll = connectToDB.database.getCollection("accounts");
         MongoCursor singleCursor = coll.find().iterator();
         while(singleCursor.hasNext()){
             System.out.println(singleCursor.next());
         }
-
-
-
-
     }
 }
